7883ecd6613621d9f49d8ff4bc959b45d3c4a980
bhartivikas <bhartivikas2427@gmail.com>
2023-08-06 20:47:19 +0530

    Added and Updated few files

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,30 @@
 resource "aws_instance" "terraform_instance" {
   ami           = "ami-0ded8326293d3201b"
   instance_type = "t2.micro"
+  key_name      = "tf-key-pair"
 
   tags = {
     Name = "terraform_instance"
   }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("./tf-key-pair.pem")
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'wait until SSH is ready'",
+
+
+      "sudo yum upgrade -y",
+      "sudo yum install docker -y",
+
+      "sudo service docker start",
+      "sudo service docker status"
+    ]
+  }
+
+
 }
